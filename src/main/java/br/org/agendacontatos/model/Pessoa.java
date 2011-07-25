aa6fb7243018d791d55cfecc7fdb581069e728fb
Ricardo M. Oliveira <ricardo.martinelli.oliveira@gmail.com>
2011-07-25 19:02:55 -0300

    Projeto Inicial

---
+++ src/main/java/br/org/agendacontatos/model/Pessoa.java
@@ -0,0 +1,92 @@
+package br.org.agendacontatos.model;
+
+import java.util.Date;
+
+public class Pessoa {
+	
+    private int id;
+    
+    private String nome;
+    
+    private String telefone;
+    
+    private String email;
+    
+    private String endereco;
+    
+    private Date dataCriacao;
+    
+    private String url;
+    
+    public Pessoa(String nome, String telefone, String email, String url,
+            String endereco, Date dataCriacao) {
+        super();
+        this.nome = nome;
+        this.telefone = telefone;
+        this.email = email;
+        this.url = url;
+        this.endereco = endereco;
+        this.dataCriacao = dataCriacao;
+    }
+
+    public Pessoa() {
+        super();
+    }
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getTelefone() {
+		return telefone;
+	}
+
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getEndereco() {
+		return endereco;
+	}
+
+	public void setEndereco(String endereco) {
+		this.endereco = endereco;
+	}
+
+	public Date getDataCriacao() {
+		return dataCriacao;
+	}
+
+	public void setDataCriacao(Date dataCriacao) {
+		this.dataCriacao = dataCriacao;
+	}
+
+	public String getUrl() {
+		return url;
+	}
+
+	public void setUrl(String url) {
+		this.url = url;
+	}
+
+}
