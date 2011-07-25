5a1e13eb36a75cb902948b2bdb45af5dc8fdc227
Ricardo M. Oliveira <ricardo.martinelli.oliveira@gmail.com>
2011-07-25 20:05:05 -0300

    Criando entidade Pessoa e testando

---
+++ src/test/java/br/org/agendacontato/test/PessoaTest.java
@@ -0,0 +1,62 @@
+package br.org.agendacontato.test;
+
+import static org.junit.Assert.assertNotSame;
+
+import java.util.Collections;
+import java.util.Date;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import br.org.agendacontatos.model.Pessoa;
+
+public class PessoaTest {
+
+	private EntityManagerFactory emf;
+
+	private EntityManager em;
+
+	private Pessoa pessoa;
+
+	@Before
+	public void preparaTeste() {
+		emf = Persistence.createEntityManagerFactory("AgendaContatos");
+		em = emf.createEntityManager();
+		pessoa = new Pessoa("Jesuino", "5556960", "jesuino@gmail.com",
+				"http://jesuino.com.br", "Rua java, 100", new Date());
+	}
+
+	@After
+	public void finalizaTeste() {
+		em.getTransaction().begin();
+		em.persist(pessoa);
+		em.getTransaction().commit();
+		assertNotSame(Collections.EMPTY_LIST, em.createQuery("from Pessoa").getResultList());
+	}
+
+	@Test
+	public void criaPessoa() {
+
+	}
+
+	@Test
+	public void alteraPessoa() {
+
+	}
+
+	@Test
+	public void removePessoa() {
+
+	}
+
+	@Test
+	public void buscaPessoa() {
+
+	}
+
+}
