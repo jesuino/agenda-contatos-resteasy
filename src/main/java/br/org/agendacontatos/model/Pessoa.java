6569b0db3b55a10a19d5a7316b2472897487de97
Ricardo M. Oliveira <ricardo.martinelli.oliveira@gmail.com>
2011-07-25 20:04:48 -0300

    Criando entidade Pessoa e testando

--- src/main/java/br/org/agendacontatos/model/Pessoa.java
+++ src/main/java/br/org/agendacontatos/model/Pessoa.java
@@ -2,8 +2,15 @@ package br.org.agendacontatos.model;
 
 import java.util.Date;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
 public class Pessoa {
 	
+	@Id
+	@GeneratedValue
     private int id;
     
     private String nome;
